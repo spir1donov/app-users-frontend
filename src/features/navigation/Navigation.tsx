bfcabff1924332998427ea735abf7dc046b85d34
Nikita <nikita@sprdnv.com>
2022-06-28 12:48:46 +0200

    add navigation

---
+++ src/features/navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import React, { FC, Key, ReactNode } from 'react'
+import { Menu, MenuProps } from 'antd'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+type MenuItem = Required<MenuProps>['items'][number]
+
+function getItem(label: ReactNode, key: Key): MenuItem {
+  return {
+    key,
+    label,
+  } as MenuItem
+}
+
+const items: MenuProps['items'] = [
+  getItem('Главная страница', '/'),
+  getItem('Профиль', '/account'),
+  getItem('Аккаунты', '/people'),
+]
+
+const Navigation: FC = () => {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const onClick: MenuProps['onClick'] = e => {
+    const { key } = e
+    navigate(key)
+  }
+
+  return <Menu onClick={onClick} style={{ width: 256 }} defaultSelectedKeys={[pathname]} mode='inline' items={items} />
+}
+
+export default Navigation
