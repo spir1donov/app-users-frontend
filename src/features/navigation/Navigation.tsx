2100a8e21269abfdaf5dd387bc9a187579f23df0
Nikita <nikita@sprdnv.com>
2022-06-28 13:23:29 +0200

    add icons

--- src/features/navigation/Navigation.tsx
+++ src/features/navigation/Navigation.tsx
@@ -1,20 +1,22 @@
 import React, { FC, Key, ReactNode } from 'react'
 import { Menu, MenuProps } from 'antd'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { ContactsOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons'
 
 type MenuItem = Required<MenuProps>['items'][number]
 
-function getItem(label: ReactNode, key: Key): MenuItem {
+function getItem(label: ReactNode, key: Key, icon?: ReactNode): MenuItem {
   return {
     key,
     label,
+    icon,
   } as MenuItem
 }
 
 const items: MenuProps['items'] = [
-  getItem('Главная страница', '/'),
-  getItem('Профиль', '/account'),
-  getItem('Аккаунты', '/people'),
+  getItem('Главная страница', '/', <HomeOutlined />),
+  getItem('Профиль', '/account', <UserOutlined />),
+  getItem('Аккаунты', '/people', <ContactsOutlined />),
 ]
 
 const Navigation: FC = () => {
